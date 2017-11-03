b58c784022c1154f6511eff750b361366eeb86f8
TDesai <TDesai@activehealth.net>
2017-11-03 16:30:11 -0400

    kafka consumer

---
+++ KafkaConsumer/src/main/java/net/ahm/careengine/CEKafkaConsumer.java
@@ -0,0 +1,50 @@
+package net.ahm.careengine;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.streams.kstream.KStreamBuilder;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CEKafkaConsumer {
+
+	private static final Logger log = LogManager.getLogger(CEKafkaProducer.class.getName());
+
+	public static void main(String[] args) throws UnknownHostException {
+		String topicName = "CEmemberRun";
+		log.info("Topic name is: {}", topicName);
+
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "nycuvig1kafka01.ahmcert.com:9092");
+		props.put("group.id", InetAddress.getLocalHost().getHostName().toString() + "TD");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.put("security.protocol", "SASL_PLAINTEXT");
+		props.put("sasl.kerberos.service.name", "kafka");
+		
+		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+		consumer.subscribe(Arrays.asList(topicName));
+		
+		while (true) {
+			ConsumerRecords<String, String> records = consumer.poll(100);
+
+			for (ConsumerRecord<String, String> record : records) {
+				System.out.println(
+						"Offset = " + record.offset() + ", Value = " + record.value());
+			}
+
+		}
+
+	}
+}
