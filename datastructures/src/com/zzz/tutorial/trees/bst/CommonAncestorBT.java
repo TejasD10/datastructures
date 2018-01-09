fabb65ff5ce06021685e217db62d7dfc77cd5098
TejasD10 <tejas.soft10@gmail.com>
2018-01-09 15:59:55 -0500

    Fixed Common Ancestor in a Binary Tree

--- datastructures/src/com/zzz/tutorial/trees/bst/CommonAncestorBT.java
+++ datastructures/src/com/zzz/tutorial/trees/bst/CommonAncestorBT.java
@@ -1,52 +1,127 @@
 package com.zzz.tutorial.trees.bst;
 
-import java.util.Arrays;
-
-import com.zzz.tutorial.trees.bst.TreeNode.Node;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Find the common Ancestor in a Binary Tree (NON BST)
- * 
  */
 public class CommonAncestorBT<E extends Comparable> {
 
-	TreeNode<Integer> tree = new TreeNode<Integer>();
+    private Node root = null;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        CommonAncestorBT<Integer> tree = new CommonAncestorBT<>();
+        tree.root = new Node(1, null, null, null);
+        tree.insertLeft(tree.root, 2);
+        tree.insertLeft(tree.root.left, 4);
+        tree.insertRight(tree.root.left, 5);
+        tree.insertRight(tree.root, 3);
+        tree.insertLeft(tree.root.right, 6);
+        tree.insertRight(tree.root.right, 7);
+        tree.levelOrder();
+        Node result = tree.commonAncestor(tree.root, 2, 4);
+        System.out.println(result == null ? "NULL" : "\n" + result.getElement());
+    }
 
-	}
+    private void insertLeft(Node root, E data) {
+        Node newNode = new Node(data, null, null, root);
+        root.setLeft(newNode);
+    }
 
-	public Node commonAncestor(E a, E b) {
-		// Search if both the nodes are present in the binary tree
-		Node A = search(tree.getRoot(), a);
-		Node B = search(tree.getRoot(), b);
+    private void insertRight(Node root, E data) {
+        Node newNode = new Node(data, null, null, root);
+        root.setRight(newNode);
+    }
 
-		if (A == null || B == null) {
-			return null;
-		}
-		Node parentA = A.getParent();
-		Node parentB = B.getParent();
+    private void levelOrder() {
+        if (root == null) {
+            System.out.println("Empty Tree");
+            return;
+        }
+        Queue<Node> q = new LinkedList<Node>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            Node temp = q.poll();
+            System.out.print(temp.getElement() + " ");
+            if (temp.getLeft() != null)
+                q.offer(temp.getLeft());
+            if (temp.getRight() != null)
+                q.offer(temp.getRight());
+        }
+    }
 
-		while (parentA != null || parentB != null) {
-			if (parentA.getElement().compareTo(parentB.getElement()) == 0) {
-				break;
-			}
-			parentA = parentA.getParent();
-			parentB = parentB.getParent();
-		}
-		return parentA;
-	}
+    public Node commonAncestor(Node root, E a, E b) {
+        if (root == null)
+            return null;
+        if (root.getElement().compareTo(a) == 0 || root.getElement().compareTo(b) == 0)
+            return root;
+        Node left = commonAncestor(root.getLeft(), a, b);
+        Node right = commonAncestor(root.getRight(), a, b);
 
-	private Node search(Node root, E a) {
-		if (root == null) {
-			return null;
-		}
-		if (root.getElement().compareTo(a) == 0) {
-			return root;
-		}
-		Node temp = search(root.getLeft(), a);
-		if (temp == null)
-			return search(root.getRight(), a);
-		return temp;
-	}
+        if (left != null && right != null)
+            return root;
+        if (left != null && right == null)
+            return left;
+        return right;
+    }
+
+    private Node search(Node root, E a) {
+        if (root == null) {
+            return null;
+        }
+        if (root.getElement().compareTo(a) == 0) {
+            return root;
+        }
+        Node temp = search(root.getLeft(), a);
+        if (temp == null)
+            return search(root.getRight(), a);
+        return temp;
+    }
+
+    private static class Node<E extends Comparable> {
+        E element;
+        Node left;
+        Node right;
+        Node parent;
+
+        Node(E element, Node left, Node right, Node parent) {
+            this.element = element;
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public Node getLeft() {
+            return left;
+        }
+
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+
+        public Node getRight() {
+            return right;
+        }
+
+        public void setRight(Node right) {
+            this.right = right;
+        }
+
+        public Node getParent() {
+            return parent;
+        }
+
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+    }
 }
