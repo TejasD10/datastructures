9bd41656b75bfce53dddc6272abe4c06ace51a0a
TejasD10 <tejas.soft10@gmail.com>
2019-08-14 17:36:33 -0400

    Kafka Consumer Updates

--- KafkaConsumer/src/main/java/net/ahm/careengine/CEKafkaConsumerAAPreauth.java
+++ KafkaConsumer/src/main/java/net/ahm/careengine/CEKafkaConsumerAAPreauth.java
@@ -1,53 +1,54 @@
 package net.ahm.careengine;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.Properties;
-
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CEKafkaConsumer {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Properties;
 
-    private static final Logger log = LogManager.getLogger(CEKafkaConsumer.class.getName());
+public class CEKafkaConsumerAAPreauth {
+
+    private static final Logger log = LogManager.getLogger(net.ahm.careengine.CEKafkaConsumer.class.getName());
 
     public static void main(String[] args) throws UnknownHostException, InterruptedException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS z");
-        String topicName[] = {"CECommunications_PHR"};
+        String topicName[] = {"aapreauth"};
         log.info("Topic name is: {}", topicName);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS z");
 
         Properties props = new Properties();
-//        props.put("bootstrap.servers", "azauvprdkafka01.ahmcert.com:9092, azauvprdkafka02.ahmcert.com:9092");
+        props.put("bootstrap.servers", "azauvprdkafka01.ahmcert.com:9092, azauvprdkafka02.ahmcert.com:9092");
 //        props.put("bootstrap.servers", "azbuvqa1kafka01.ahmcert.com:9092, azbuvqa1kafka02.ahmcert.com:9092");
 //        props.put("bootstrap.servers", "azbuvqa2kafka01.ahmcert.com:9092, azbuvqa2kafka02.ahmcert.com:9092");
-        props.put("bootstrap.servers", "azbuvqa1kafka01.ahmcert.com:9092");
-        props.put("group.id", "ALPTDESAI7136TD5");// InetAddress.getLocalHost().getHostName() + "TD");// + (int) (Math.random() * 10));
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
+//        props.put("bootstrap.servers", "azbuvdv1kafka01.ahmcert.com:9092");
+        props.put("group.id", InetAddress.getLocalHost().getHostName() + "TD-" + (int) (Math.random() * 10));
+        props.put("enable.auto.commit", "false");
+//        props.put("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("security.protocol", "SASL_PLAINTEXT");
         props.put("auto.offset.reset", "earliest");
         props.put("sasl.kerberos.service.name", "kafka");
-
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+//        consumer.seekToBeginning();
         consumer.subscribe(Arrays.asList(topicName));
-
+//        consumer.commitAsync();
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records) {
-                System.out.println("Received Message");
+//                System.out.println(
+//                        "Offset = " + record.offset() + ", Value = " + record.value());
                 log.info(
-                        record.timestampType() + " = " + dateFormat.format(new Date(record.timestamp())) + ", Offset = " + record.offset() + ", Value = " + record.value());
+                        record.timestampType() + " = " + dateFormat.format(new Date(record.timestamp())) + ", Offset = " + record.offset() + ", Value = " + record.value() );
             }
         }
     }
+
 }
--- KafkaConsumer/src/main/java/net/ahm/careengine/CEKafkaProducer.java
+++ KafkaConsumer/src/main/java/net/ahm/careengine/CEKafkaProducer.java
@@ -1,5 +1,8 @@
 package net.ahm.careengine;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Random;
 
@@ -15,13 +18,15 @@ public class CEKafkaProducer {
 	private static final Logger log = LogManager.getLogger(CEKafkaProducer.class.getName());
 	private static KafkaProducer<Integer, String> producer;
 
-	public static void main(String[] args) {
-		String topicName = "MemberDataNewEvents";
+	public static void main(String[] args) throws IOException {
+		String topicName = "CEmemberRun";
 		log.info("Topic name is: {}", topicName);
 		Properties props = new Properties();
 
 //		props.put("bootstrap.servers", "azauvprdkafka01.ahmcert.com:9092, azauvprdkafka02.ahmcert.com:9092");
-		props.put("bootstrap.servers", "azbuvqa1kafka01.ahmcert.com:9092");
+		props.put("bootstrap.servers", "azbuvqa1kafka01.ahmcert.com:9092, azbuvqa1kafka02.ahmcert.com:9092");
+//		props.put("bootstrap.servers", "azbuvqa2kafka01.ahmcert.com:9092, azbuvqa2kafka02.ahmcert.com:9092");
+//		props.put("bootstrap.servers", "azbuvdv1kafka01.ahmcert.com:9092");
 		// props.put("acks", "all");
 		props.put("retries", 0);
 		props.put("batch.size", 16384);
@@ -31,37 +36,40 @@ public class CEKafkaProducer {
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("security.protocol", "SASL_PLAINTEXT");
 		props.put("sasl.kerberos.service.name", "kafka");
-		props.put("max.block.ms", "2000");
+		props.put("max.block.ms", "20000");
 
 		try {
-			producer = new KafkaProducer<Integer, String>(props);
-			System.out.println("Kafka Producer created");
-			String[] jsonStrings = new String[20];
-			int N = 1;
-			Random rand = new Random();
-			for (int i = 0; i < N; i++) {
-				jsonStrings[i] = "{\"memberPlanId\" : 68962040,\"memberId\" : 64262706,\"eventTypes\" : [ \"\" ],\"accountId\" : null,\"supplierId\" : 6301,\"eventSource\" : \"SS_UE\"}";
-			}
-			//String jsonString = "{\"memberPlanId\" : 72860305,\"memberId\" : 64709710,\"eventTypes\" : [ \"LAB\", \"CLINICAL\" ],\"accountId\" : null,\"supplierId\" : null,\"eventSource\" : \"AA\"}";
-			for (int i = 0; i < N; i++) {
-				producer.send(new ProducerRecord<Integer, String>(topicName, 0, i, jsonStrings[i]), new Callback() {
+            producer = new KafkaProducer<Integer, String>(props);
+            System.out.println("Kafka Producer created");
+            String[] jsonStrings = new String[20];
+            int N = 1;
+            Random rand = new Random();
+            for (int i = 0; i < N; i++) {
+//				jsonStrings[i] = "{\"memberPlanId\" : 68962040,\"memberId\" : 64262706,\"eventTypes\" : [ \"\" ],\"accountId\" : null,\"supplierId\" : 6301,\"eventSource\" : \"SS_UE\"}";
+                jsonStrings[i] = Files.readAllLines(Paths.get("../resources/Input.txt")).get(0);
+            }
 
-					public void onCompletion(RecordMetadata metaData, Exception exception) {
-						if (exception != null) {
-							System.out.println("Exception Occured" + exception.getMessage());
-							return;
-						}
-						System.out.println("Message Sent on " + metaData.topic() + " ,partion " + metaData.partition()
-								+ " ,offset " + metaData.offset());
-					}
-				});
-				try {
-					Thread.sleep(7000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		} finally {
+            //String jsonString = "{\"memberPlanId\" : 72860305,\"memberId\" : 64709710,\"eventTypes\" : [ \"LAB\", \"CLINICAL\" ],\"accountId\" : null,\"supplierId\" : null,\"eventSource\" : \"AA\"}";
+            for (int i = 0; i < N; i++) {
+                producer.send(new ProducerRecord<Integer, String>(topicName, 0, i, jsonStrings[i]), new Callback() {
+
+                    public void onCompletion(RecordMetadata metaData, Exception exception) {
+                        if (exception != null) {
+                            System.out.println("Exception Occured: " + exception.getMessage());
+                            return;
+                        }
+                        System.out.println("Message Sent on " + metaData.topic() + " ,partion " + metaData.partition()
+                                + " ,offset " + metaData.offset());
+                    }
+                });
+//				try {
+//					Thread.sleep(7000);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+            }
+        }
+		 finally {
 			if (producer != null)
 				producer.close();
 		}
